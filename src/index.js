84ca14d42fbb670494081353dc626e437d8249fb
Hypermask Team <team@hypermask.io>
2018-10-07 07:42:38 -0700

    supporting multiple diferent chains

--- src/index.js
+++ src/index.js
@@ -41,12 +41,23 @@ import Web3 from 'web3'
 import Tx from 'ethereumjs-tx'
 import fetch from 'isomorphic-fetch'
 
-var web3 = new Web3(
-    new Web3.providers.HttpProvider('https://ropsten.infura.io/')
-);
+var web3Ropsten = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
+var web3Main = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/'));
+var web3Rinkeby = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/'));
+var web3Kovan = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/'));
 
-function sendRaw(rawTx) {
+
+function getWeb3(chain){
+    if(chain === 'ropsten') return web3Ropsten;
+    if(chain === 'mainnet') return web3Main;
+    if(chain === 'rinkeby') return web3Rinkeby;
+    if(chain === 'kovan') return web3Kovan;
+    throw new Error('Unknown chain: ' + chain)
+}
+
+function sendRaw(chain, rawTx) {
     return new Promise((resolve, reject) => {
+        let web3 = getWeb3(chain);
         var privateKey = new Buffer(process.env.ETHEREUM_KEY, 'hex');
         var transaction = new Tx(rawTx);
         transaction.sign(privateKey);
@@ -58,9 +69,11 @@ function sendRaw(rawTx) {
 }
 
 
-async function sendToAddress(amount, recipientAddress){
+async function sendToAddress(chain, amount, recipientAddress){
     console.log(`Sending ${amount} wei to ${recipientAddress}`)
-    return sendRaw({
+    let web3 = getWeb3(chain);
+
+    return sendRaw(chain, {
         "from": process.env.ETHEREUM_ADDRESS, 
         "gasPrice": web3.utils.numberToHex(await web3.eth.getGasPrice()),
         "gasLimit": web3.utils.toHex(210000), // 21,000 is standard tx gas limit
@@ -87,19 +100,40 @@ app.get('/widget', async (req, res) => {
     const usdAmount = req.query.amount;
     const ethAmount = usdAmount / ethPrice;
 
-    res.render('widget', {
-        title: 'wumbo',
-        STRIPE_KEY: process.env.STRIPE_CLIENT_KEY,
+    // console.assert(+usdAmount < 25, 'Transaction can not be more than $25')
+
+    const data = {
+        stripe_key: process.env.STRIPE_CLIENT_KEY,
         eth_address: req.query.address,
         eth_amount: ethAmount,
         usd_amount: req.query.amount,
+        chain: req.query.chain,
         source: req.query.source || 'N/A'
+    }
+
+    let web3 = getWeb3(req.query.chain)
+    let currentBalance = await web3.eth.getBalance(process.env.ETHEREUM_ADDRESS, 'pending');
+
+    const BN = x => web3.utils.toBN(x || '0');
+
+    let weiAmount = web3.utils.toWei(ethAmount.toString(), 'ether');
+
+    if(BN(currentBalance).lt(BN(weiAmount))){
+        throw new Error('insufficient funds')
+    }
+
+
+    res.render('widget', {
+        ...data,
+        b64json: new Buffer(JSON.stringify(data)).toString('base64')
     })
 })
 
 
 app.post('/charge', async (req, res) => {
     let ethPrice = await getETHUSDPrice();
+    let web3 = getWeb3(req.body.chain)
+
     console.log(ethPrice);
     console.log(req.body)
     let amount = req.body.usd_amount;
@@ -114,14 +148,15 @@ app.post('/charge', async (req, res) => {
         source: req.body.token,
     });
 
+
+
     console.log(charge)
     const recipientAddress = req.body.address;
 
     let transactionHash = await sendToAddress(
+        req.body.chain, 
         web3.utils.toWei(ethAmount.toString(), 'ether'), 
         recipientAddress)
-
-    // console.log(result)
 
     res.end(JSON.stringify({
         transactionHash: transactionHash
